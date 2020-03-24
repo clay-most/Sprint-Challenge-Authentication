607612335ef82c400dfcb751badcf0c911632464
Anna Morris <annapatton444@gmail.com>
2020-03-24 12:51:08 -0500

    joke endpoint closer to working

--- tests/register.test.js
+++ tests/register.test.js
@@ -11,11 +11,11 @@ test("test good register", async () => {
 });
 
 test("test duplicate", async () => {
-    const res = await supertest(server)
-      .post("/api/auth/register")
-      .send({ username: "Artemis", password: "Fowl" });
-    expect(res.statusCode).toBe(500);
-  });
+  const res = await supertest(server)
+    .post("/api/auth/register")
+    .send({ username: "Artemis", password: "Fowl" });
+  expect(res.statusCode).toBe(500);
+});
 
 test("test good login", async () => {
   const res = await supertest(server)
@@ -25,8 +25,22 @@ test("test good login", async () => {
 });
 
 test("test misspelling", async () => {
-    const res = await supertest(server)
-      .post("/api/auth/register")
-      .send({ username: "Artemis", password: "Foul" });
-    expect(res.statusCode).toBe(500);
-  });
+  const res = await supertest(server)
+    .post("/api/auth/register")
+    .send({ username: "Artemis", password: "Foul" });
+  expect(res.statusCode).toBe(500);
+});
+
+// test("get all jokes", async () => {
+//   const res = await supertest(server)
+//     .post("/api/auth/login")
+//     .send({ username: "Artemis", password: "Fowl" });
+
+//   const res2 = await supertest(server)
+//     .get("/api/auth/jokes")
+//     .send({ authorization: res.body.token });
+
+//   console.log(res.body.token);
+//   expect(res2.statusCode).toBe(200);
+//   expect(res2.type).toBe("application/json");
+// });
